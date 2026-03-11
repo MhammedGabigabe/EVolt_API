5441b90d16e3d1ccdcaa852c752d819b55735155
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-11 14:12:20 +0000

    feat: génération du modèle SessionCharge avec scaffold API complet

---
+++ app/Http/Controllers/SessionChargeController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SessionCharge;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSessionChargeRequest;
+use App\Http\Requests\UpdateSessionChargeRequest;
+
+class SessionChargeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSessionChargeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SessionCharge $sessionCharge)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSessionChargeRequest $request, SessionCharge $sessionCharge)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SessionCharge $sessionCharge)
+    {
+        //
+    }
+}
