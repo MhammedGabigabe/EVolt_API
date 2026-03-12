916a1f33cf9b7429eb97b0bb035ac51f4166b4aa
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-12 12:06:54 +0000

    feat: ajout des fillable et relations du modèle SessionCharge

--- app/Models/SessionCharge.php
+++ app/Models/SessionCharge.php
@@ -9,4 +9,16 @@ class SessionCharge extends Model
 {
     /** @use HasFactory<\Database\Factories\SessionChargeFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'reservation_id',
+        'debut_session',
+        'fin_session',
+        'energie_delivree'
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
