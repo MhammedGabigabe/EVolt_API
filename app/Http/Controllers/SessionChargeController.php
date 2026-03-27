660f377785979732bd6468a9d010fd4525e94896
Gabigabe <mhammed.gabigabe@gmail.com>
2026-03-27 16:23:16 +0100

    feat(session_charge): implementer le code pour commencer une session

--- app/Http/Controllers/SessionChargeController.php
+++ app/Http/Controllers/SessionChargeController.php
@@ -3,11 +3,49 @@
 namespace App\Http\Controllers;
 
 use App\Models\SessionCharge;
+use App\Models\Reservation;
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class SessionChargeController extends Controller
 {
+
+
+    public function startSession($reservationId)
+    {
+        DB::beginTransaction();
+
+        try{
+            $reservation = Reservation::findOrFail($reservationId);
+
+            if ($reservation->sessionCharge) {
+                return response()->json([
+                    'message' => 'Session déjà démarrée'
+                ], 400);
+            }
+            $session = SessionCharge::create([
+                'reservation_id' => $reservation->id,
+                'debut_session' => now(),
+                'fin_session' => null,
+            ]);
+
+            $reservation->update([
+                'statut' => 'active'
+            ]);
+
+            DB::commit();
+
+            return response()->json($session, 201);
+        }catch(Exception $e){
+            DB::rollback();
+            return response()->json([
+                'message' => 'Erreur lors du démarrage de la session',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
+
     public function mySessions()
     {
         $user = auth()->user();
@@ -36,25 +74,15 @@ class SessionChargeController extends Controller
     {
         $borne = $session->reservation->borne;
 
-        $debut = Carbon::parse($session->debut_session);
-
-        $fin = $session->fin_session
-            ? Carbon::parse($session->fin_session)
-            : now();
-
-        $dureeMinutes = $debut->diffInMinutes($fin);
-        $dureeHeures = $dureeMinutes / 60;
-
-        $energie = $borne->puissance_kw * $dureeHeures;
-
         return [
             'id' => $session->id,
             'borne' => $borne->nom,
             'localisation' => $borne->localisation,
             'debut' => $session->debut_session,
             'fin' => $session->fin_session,
-            'energie delivree' => round($energie, 2),
+            'energie delivree' => $session->energie_delivree,
             'statut' => is_null($session->fin_session) ? 'en cours' : 'terminée'
         ];
     }
+
 }
